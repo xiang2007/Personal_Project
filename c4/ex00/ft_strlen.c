489af83fa5da88b95161c7156bb5d6b06ef4aa6a
Wee Shou Xiang <wshou-xi@u81z09s03.42kl.edu.my>
2025-03-17 20:46:20 +0800

    small change

--- c4/ex00/ft_strlen.c
+++ c4/ex00/ft_strlen.c
@@ -1,22 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshou-xi <wshou-xi@student.42kl.edu.m      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 11:08:25 by wshou-xi          #+#    #+#             */
+/*   Updated: 2025/03/17 11:21:52 by wshou-xi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_strlen(char *str)
 {
 	int	i;
-	int	count;
 
 	i = 0;
-	count = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if ((str[i] >= 'A' && str[i] <= 'Z')
-			|| (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			i++;
-			count++;
-		}
-		else
-		{
-			i++;
-		}
-		return (count);
+		i++;
 	}
+	return (i);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	char s1[50] = "8237273HELLO";
+	printf("%d", ft_strlen(s1));
+	return (0);
+}*/
--- c4/ex01/ft_putstr.c
+++ c4/ex01/ft_putstr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wshou-xi <wshou-xi@student.42kl.edu.m      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 20:43:30 by wshou-xi          #+#    #+#             */
+/*   Updated: 2025/03/17 20:43:33 by wshou-xi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putstr(char *str)
@@ -5,9 +17,14 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-				i++;
-	}
+	while (str[i])
+		i++;
 	write(1, str, i);
 }
+/*
+int main(void)
+{
+	char str[50] = "hello here";
+	ft_putstr(str);
+	return (0);
+}*/
