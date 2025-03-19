d5aa0ebade5d118c8d618821820f6307f94ca648
Wee Shou Xiang <wshou-xi@u81z09s03.42kl.edu.my>
2025-03-19 09:01:45 +0800

    added all

--- c3/ex01/ft_strncmp.c
+++ c3/ex01/ft_strncmp.c
@@ -14,6 +14,8 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while ((i < n - 1) && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
--- c3/ex03/ft_strncat.c
+++ c3/ex03/ft_strncat.c
@@ -19,14 +19,12 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	j = 0;
 	while (dest[i])
 		i++;
-	while (src[j])
+	while (src[j] && j < nb)
 	{
-		if (j < nb)
-		{
-			dest[i] = src[j];
-			i++;
-		}
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
