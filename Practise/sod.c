894db506f00f969832e3038af74901b203abed88
Shou Xiang <weeshouxiang@gmail.com>
2025-04-11 21:48:44 +0800

    minor change

--- Practise/sod.c
+++ Practise/sod.c
@@ -1,26 +1,13 @@
 #include <stdlib.h>
 
-int sum_of_digit(int size,char *digit)
+int sum_of_digit(int n)
 {
-    digit = (char *)malloc(size *sizeof(char));
-    int i;
-    int count;
-    count = 0;
-    i = 0;
-    while (digit[i])
-    {
-        return (count += digit[i]);
-        i++;
-    }
 
 }
 
 #include <stdio.h>
-#include <string.h>
-int main(int argc, char **argv)
+int main()
 {
-    int s;
-    s = strlen(argv[1]);
-    pritnf("%d",sum_of_digit(s, argv[1]));
+
 
 }
--- c3/ex04/ft_strstr.c
+++ c3/ex04/ft_strstr.c
@@ -21,9 +21,9 @@ char	*ft_strstr(char *str, char *to_find)
 	while (str[i] != '\0')
 	{
 		j = 0;
-		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		while (str[i + j] == to_find[j] && !to_find[j])
 			j++;
-		if (to_find[j] == '\0')
+		if (to_find[j])
 			return (&str[i]);
 		i++;
 	}
